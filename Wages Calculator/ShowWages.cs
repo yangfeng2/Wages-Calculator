26b83cff6d4cf03a8d5e0347614075295bba6357
Yang <yangfeng@live.com.my>
2019-12-20 22:42:38 +1100

    Test

--- Wages Calculator/ShowWages.cs
+++ Wages Calculator/ShowWages.cs
@@ -30,13 +30,14 @@ namespace Wages_Calculator
         {
             foreach(Staff s in staffList)
             {
-                data.Add("\nName: "+s.Name+", Wage Per Hour: $"+s.Wages+"\n\n");
+                data.Add("\nName: " + s.Name + ", Wage Per Hour: $" + s.Wages + "\n\n");
                 data.Add("Monday: $" + Math.Round(s.MonWages,2) + "\nTuesday: $"+s.TueWages+"\nWednesday: $"+s.WedWages+"\nThursday: $"+s.ThuWages+"\nFriday: $"+s.FriWages+"\nSaturday: $"+s.SatWages+"\nSunday: $"+s.SunWages+"");
             }
 
             foreach(string s1 in data)
             {
                 richTextBox1.AppendText(s1);
+                Console.WriteLine();
             }
         }
     }
